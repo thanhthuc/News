6aa9b45a3f5725f6fd7418cdb6eb3a68e2c52fc1
Thanh Thuc <thanhthuc.ng@samsung.com>
2023-07-25 20:00:44 +0700

    [Flutter] Bloc orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((value) {
+    runApp(const MyApp());
+  });
 
-  runApp(const MyApp());
-
+  // runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
