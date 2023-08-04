f60e9ba634d848a503e2f56aafbfcb120f77520b
Thanh Thuc <thanhthuc.ng@samsung.com>
2023-08-04 18:00:35 +0700

    [Flutter] Add api client

--- lib/api_client/api_client.dart
+++ lib/api_client/api_client.dart
@@ -4,29 +4,46 @@
 // Multi inherited
 // https://developermemos.com/posts/dart-abstract-classes-interfaces
 
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:news/model/news_model.dart';
+import 'package:http/http.dart';
 
 abstract class BaseAPI {
   String get baseURL;
-  void fetchData(String responseBody);
+  Future<List<News>> fetchNews(Client client);
 }
 
 abstract class Parse {
-  void parse();
+  List<News> parseNewsFrom(String jsonResponseBody);
 }
 
-class APIClient implements BaseAPI {
+class APIClient implements BaseAPI, Parse {
   // https://docs.flutter.dev/cookbook/networking/background-parsing
   // What is isolate?
-  @override
-  List<News> fetchData(String responseBody) {
-    // TODO: implement fetchData
 
-    return [];
-  }
+  String apiKey = "8ccb67aaefd4475ba20a6bb2ef79a35d";
 
   @override
   // TODO: implement baseURL
-  String get baseURL => "baseURL";
+  // GET https://newsapi.org/v2/everything?q=Apple&from=2023-08-03&sortBy=popularity&apiKey=API_KEY
+  String get baseURL => "https://newsapi.org/v2/everything?q=Apple&from=2023-08-03&sortBy=popularity&apiKey=$apiKey";
+
+  @override
+  Future<List<News>> fetchNews(Client client) async {
+    final response = await client.get(Uri.parse(baseURL));
+    // List<News> list = parseNewsFrom(response.body); => app freeze for a moment
+    // Use compute to isolate to background thread
+    return compute(parseNewsFrom, response.body);
+  }
+
+  @override
+  List<News> parseNewsFrom(String jsonResponseBody) {
+    // TODO: implement parseNewsFrom
+    final parsed = jsonDecode(jsonResponseBody).cast<Map<String, dynamic>>();
+    var listNews = parsed.map<News>((json) => News.fromJson(json as Map<String, dynamic>)).toList();
+    return listNews;
+  }
 
 }
